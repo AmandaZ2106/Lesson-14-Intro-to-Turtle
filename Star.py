1a0e9f7b1414066a974232a12666fc6d0a2ee58d
AmandaZ2106 <romeogcruz@outlook.com>
2025-09-17 19:33:01 +0100

    Star

---
+++ Star.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(300,400)
+pen=turtle.Turtle()
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.penup()
+pen.right(150)
+pen.forward(50)
+pen.down()
+pen.right(90)
+pen.forward(100)
+pen.right(120)
+pen.forward(100)
+pen.right(120)
+pen.forward(100)
+turtle.done()
+
+
+
+
+
+    
+    
